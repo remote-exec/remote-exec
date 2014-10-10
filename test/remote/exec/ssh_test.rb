c6052aceec112d5cf3fdd14360207cd5d4751e78
Michal Papis <mpapis@gmail.com>
2014-10-10 21:08:28 +0200

    add test for calling the initialize block once, view with -w

--- test/remote/exec/ssh_test.rb
+++ test/remote/exec/ssh_test.rb
@@ -46,79 +46,90 @@ describe Remote::Exec::Ssh do
     Remote::Exec::Ssh.allocate.tap{|ssh| ssh.instance_variable_set(:@ssh, connection)}
   end
 
-  it "sets default variables" do
-    subject.send(:initialize, 1, 2, 3)
-    subject.host.must_equal 1
-    subject.user.must_equal 2
-    subject.options.must_equal 3
-  end
-
-  it "executes true" do
-    story do |session|
-      channel = session.opens_channel
-      channel.sends_request_pty
-      channel.sends_exec "true"
-      channel.gets_exit_status(0)
-      channel.gets_close
-      channel.sends_close
+  describe "#initialize" do
+    it "sets default variables" do
+      subject.send(:initialize, 1, 2, 3)
+      subject.host.must_equal 1
+      subject.user.must_equal 2
+      subject.options.must_equal 3
     end
 
-    assert_scripted do
-      subject.execute("true").must_equal 0
+    it "executes initialize block once" do
+      calls = 0
+      subject.send(:initialize, 1, 2, 3) { calls+=1 }
+      calls.must_equal 1
     end
-  end
+  end #initialize
 
-  it "executes false" do
-    story do |session|
-      channel = session.opens_channel
-      channel.sends_request_pty
-      channel.sends_exec "false"
-      channel.gets_exit_status(1)
-      channel.gets_close
-      channel.sends_close
-    end
+  describe "#execute" do
+    it "executes true" do
+      story do |session|
+        channel = session.opens_channel
+        channel.sends_request_pty
+        channel.sends_exec "true"
+        channel.gets_exit_status(0)
+        channel.gets_close
+        channel.sends_close
+      end
 
-    assert_scripted do
-      subject.execute("false").must_equal 1
-    end
-  end
-
-  it "executes echo test" do
-    story do |session|
-      channel = session.opens_channel
-      channel.sends_request_pty
-      channel.sends_exec "echo test me"
-      channel.gets_data("test me\n")
-      channel.gets_exit_status(0)
-      channel.gets_close
-      channel.sends_close
+      assert_scripted do
+        subject.execute("true").must_equal 0
+      end
     end
 
-    assert_scripted do
-      subject.execute("echo test me") do |out, err|
-        out.must_equal "test me\n"
-        err.must_be_nil
-      end.must_equal 0
-    end
-  end
+    it "executes false" do
+      story do |session|
+        channel = session.opens_channel
+        channel.sends_request_pty
+        channel.sends_exec "false"
+        channel.gets_exit_status(1)
+        channel.gets_close
+        channel.sends_close
+      end
 
-  it "executes echo test>&2" do
-    story do |session|
-      channel = session.opens_channel
-      channel.sends_request_pty
-      channel.sends_exec "echo test me>&2"
-      channel.gets_extended_data("test me\n")
-      channel.gets_exit_status(0)
-      channel.gets_close
-      channel.sends_close
+      assert_scripted do
+        subject.execute("false").must_equal 1
+      end
     end
 
-    assert_scripted do
-      subject.execute("echo test me>&2") do |out, err|
-        out.must_be_nil
-        err.must_equal "test me\n"
-      end.must_equal 0
+    it "executes echo test" do
+      story do |session|
+        channel = session.opens_channel
+        channel.sends_request_pty
+        channel.sends_exec "echo test me"
+        channel.gets_data("test me\n")
+        channel.gets_exit_status(0)
+        channel.gets_close
+        channel.sends_close
+      end
+
+      assert_scripted do
+        subject.execute("echo test me") do |out, err|
+          out.must_equal "test me\n"
+          err.must_be_nil
+        end.must_equal 0
+      end
     end
-  end
+
+    it "executes echo test>&2" do
+      story do |session|
+        channel = session.opens_channel
+        channel.sends_request_pty
+        channel.sends_exec "echo test me>&2"
+        channel.gets_extended_data("test me\n")
+        channel.gets_exit_status(0)
+        channel.gets_close
+        channel.sends_close
+      end
+
+      assert_scripted do
+        subject.execute("echo test me>&2") do |out, err|
+          out.must_be_nil
+          err.must_equal "test me\n"
+        end.must_equal 0
+      end
+    end
+
+  end #execute
 
 end
