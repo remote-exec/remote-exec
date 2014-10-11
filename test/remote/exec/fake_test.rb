32d6c549fbebcb259e01ec5442d59fb8721f0c96
Michal Papis <mpapis@gmail.com>
2014-10-12 01:02:18 +0200

    rewrite fake_test as spec

--- test/remote/exec/fake_test.rb
+++ test/remote/exec/fake_test.rb
@@ -1,58 +1,55 @@
 require 'test_helper'
 require 'remote/exec/fake'
 
-class Remote::Exec::TestFake < MiniTest::Unit::TestCase
-  def setup
-    @test = Remote::Exec::Fake
+describe Remote::Exec::Fake do
+  subject do
+    Remote::Exec::Fake.new
   end
 
-  def test_true
+  it "runs true" do
     test_command = "true"
-    fake = @test.new
-    fake.respond do |command|
-      assert_equal command, test_command
+    subject.respond do |command|
+      command.must_equal test_command
       [0,[]]
     end
     called = 0
     status =
-    fake.execute(test_command) do |out, err|
+    subject.execute(test_command) do |out, err|
       called+=1
     end
-    assert_equal 0, called
-    assert_equal 0, status
+    called.must_equal(0)
+    status.must_equal(0)
   end
 
-  def test_false
+  it "runs false" do
     test_command = "false"
-    fake = @test.new
-    fake.respond do |command|
-      assert_equal command, test_command
+    subject.respond do |command|
+      command.must_equal test_command
       [1,[]]
     end
     called = 0
     status =
-    fake.execute(test_command) do |out, err|
+    subject.execute(test_command) do |out, err|
       called+=1
     end
-    assert_equal 0, called
-    assert_equal 1, status
+    called.must_equal(0)
+    status.must_equal(1)
   end
 
-  def test_echo_test
+  it "runs echo test" do
     test_command = "echo test"
-    fake = @test.new
-    fake.respond do |command|
-      assert_equal command, test_command
+    subject.respond do |command|
+      command.must_equal test_command
       [0,[["test\n",nil]]]
     end
     called = 0
     status =
-    fake.execute(test_command) do |out, err|
-      assert_equal out, "test\n"
-      assert_equal err, nil
+    subject.execute(test_command) do |out, err|
+      out.must_equal "test\n"
+      err.must_be_nil
       called+=1
     end
-    assert_equal 1, called
-    assert_equal 0, status
+    called.must_equal(1)
+    status.must_equal(0)
   end
 end
