f8b3bf6b40d608f709ed2f4cbd9a6909bf02bd1a
Michal Papis <mpapis@gmail.com>
2014-10-12 17:00:56 +0200

    improved the recognition of docs in base ... output is not best, but better

--- lib/remote-exec/base.rb
+++ lib/remote-exec/base.rb
@@ -12,43 +12,59 @@ class RemoteExec::Base
   extend Ruby::Hooks::InstanceHooks
 
   # called before connection attempt will be made, used when connection may fail
-  # @param object [Object] the target that invoked the method
+  # @!method before_connect
+  # @return            [Hook]   the +Observable+ implementation to handle hooks
+  # @yieldparam object [Object] the target that invoked the method
   define_hook :before_connect
 
   # called when connection attempt failed and we are about to sleep and retry
-  # @param object    [Object]    the target that invoked the method
-  # @param exception [Exception] exception that made the connection attempt fail
-  # @param retries   [Integer]   number of retries left
+  # @!method on_connect_retry
+  # @return               [Hook]      the +Observable+ implementation to handle hooks
+  # @yieldparam object    [Object]    the target that invoked the method
+  # @yieldparam exception [Exception] exception that made the connection attempt fail
+  # @yieldparam retries   [Integer]   number of retries left
   define_hook :on_connect_retry
 
   # called when connection attempt failed and we no more retries left
-  # @param object    [Object]    the target that invoked the method
-  # @param exception [Exception] exception that made the connection attempt fail
+  # @!method on_connect_fail
+  # @return               [Hook]      the +Observable+ implementation to handle hooks
+  # @yieldparam object    [Object]    the target that invoked the method
+  # @yieldparam exception [Exception] exception that made the connection attempt fail
   define_hook :on_connect_fail
 
   # called after connection / session is esatablished
-  # @param object [Object] the target that invoked the method
+  # @!method after_connect
+  # @return            [Hook]   the +Observable+ implementation to handle hooks
+  # @yieldparam object [Object] the target that invoked the method
   define_hook :after_connect
 
   # called before terminating connection - only when needed
-  # @param object [Object] the target that invoked the method
+  # @!method before_shutdown
+  # @return            [Hook]   the +Observable+ implementation to handle hooks
+  # @yieldparam object [Object] the target that invoked the method
   define_hook :before_shutdown
 
   # called before executing command
-  # @param object  [Object] the target that invoked the method
-  # @param command [String] the command to execute
+  # @!method before_execute
+  # @return             [Hook]   the +Observable+ implementation to handle hooks
+  # @yieldparam object  [Object] the target that invoked the method
+  # @yieldparam command [String] the command to execute
   define_hook :before_execute
 
   # called before executing command
-  # @param object [Object] the target that invoked the method
-  # @param stdout [String] standard output of the command, can be nil
-  # @param stderr [String] standard error output of the command, can be nil
+  # @!method on_execute_data
+  # @return            [Hook]   the +Observable+ implementation to handle hooks
+  # @yieldparam object [Object] the target that invoked the method
+  # @yieldparam stdout [String] standard output of the command, can be nil
+  # @yieldparam stderr [String] standard error output of the command, can be nil
   define_hook :on_execute_data
 
   # called after executing command
-  # @param object  [Object]  the target that invoked the method
-  # @param command [String]  the executed command
-  # @param result  [Integer] the executed command status code (0 - ok, >0 - fail)
+  # @!method after_execute
+  # @return             [Hook]    the +Observable+ implementation to handle hooks
+  # @yieldparam object  [Object]  the target that invoked the method
+  # @yieldparam command [String]  the executed command
+  # @yieldparam result  [Integer] the executed command status code (0 - ok, >0 - fail)
   define_hook :after_execute
 
   # standard in place handler that ensures shutdown is called
