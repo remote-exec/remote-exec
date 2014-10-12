f96ae6ed25883454775b76d70e76ef3e46070935
Michal Papis <mpapis@gmail.com>
2014-10-12 22:32:39 +0200

    use new yard-ruby-hooks format for describing the hooks in base

--- lib/remote-exec/base.rb
+++ lib/remote-exec/base.rb
@@ -12,59 +12,43 @@ class RemoteExec::Base
   extend Ruby::Hooks::InstanceHooks
 
   # called before connection attempt will be made, used when connection may fail
-  # @!method before_connect
-  # @return            [Hook]   the +Observable+ implementation to handle hooks
-  # @yieldparam object [Object] the target that invoked the method
+  # @notify_param object [Object] the target that invoked the method
   define_hook :before_connect
 
   # called when connection attempt failed and we are about to sleep and retry
-  # @!method on_connect_retry
-  # @return               [Hook]      the +Observable+ implementation to handle hooks
-  # @yieldparam object    [Object]    the target that invoked the method
-  # @yieldparam exception [Exception] exception that made the connection attempt fail
-  # @yieldparam retries   [Integer]   number of retries left
+  # @notify_param object    [Object]    the target that invoked the method
+  # @notify_param exception [Exception] exception that made the connection attempt fail
+  # @notify_param retries   [Integer]   number of retries left
   define_hook :on_connect_retry
 
-  # called when connection attempt failed and we no more retries left
-  # @!method on_connect_fail
-  # @return               [Hook]      the +Observable+ implementation to handle hooks
-  # @yieldparam object    [Object]    the target that invoked the method
-  # @yieldparam exception [Exception] exception that made the connection attempt fail
+  # called when connection attempt failed and no more retries left
+  # @notify_param object    [Object]    the target that invoked the method
+  # @notify_param exception [Exception] exception that made the connection attempt fail
   define_hook :on_connect_fail
 
   # called after connection / session is esatablished
-  # @!method after_connect
-  # @return            [Hook]   the +Observable+ implementation to handle hooks
-  # @yieldparam object [Object] the target that invoked the method
+  # @notify_param object [Object] the target that invoked the method
   define_hook :after_connect
 
   # called before terminating connection - only when needed
-  # @!method before_shutdown
-  # @return            [Hook]   the +Observable+ implementation to handle hooks
-  # @yieldparam object [Object] the target that invoked the method
+  # @notify_param object [Object] the target that invoked the method
   define_hook :before_shutdown
 
   # called before executing command
-  # @!method before_execute
-  # @return             [Hook]   the +Observable+ implementation to handle hooks
-  # @yieldparam object  [Object] the target that invoked the method
-  # @yieldparam command [String] the command to execute
+  # @notify_param object  [Object] the target that invoked the method
+  # @notify_param command [String] the command to execute
   define_hook :before_execute
 
   # called before executing command
-  # @!method on_execute_data
-  # @return            [Hook]   the +Observable+ implementation to handle hooks
-  # @yieldparam object [Object] the target that invoked the method
-  # @yieldparam stdout [String] standard output of the command, can be nil
-  # @yieldparam stderr [String] standard error output of the command, can be nil
+  # @notify_param object [Object] the target that invoked the method
+  # @notify_param stdout [String] standard output of the command, can be nil
+  # @notify_param stderr [String] standard error output of the command, can be nil
   define_hook :on_execute_data
 
   # called after executing command
-  # @!method after_execute
-  # @return             [Hook]    the +Observable+ implementation to handle hooks
-  # @yieldparam object  [Object]  the target that invoked the method
-  # @yieldparam command [String]  the executed command
-  # @yieldparam result  [Integer] the executed command status code (0 - ok, >0 - fail)
+  # @notify_param object  [Object]  the target that invoked the method
+  # @notify_param command [String]  the executed command
+  # @notify_param result  [Integer] the executed command status code (0 - ok, >0 - fail)
   define_hook :after_execute
 
   # standard in place handler that ensures shutdown is called
